3ea534673cc5dd9a985f1b0c5a961f9597e0f43d
jorted-runner <jorted.python@gmail.com>
2025-05-27 15:49:02 -0500

    Nav component and navigation

---
+++ src/app/ui/nav/logo.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Logo() {
+    return (
+			<Link href="/">
+				<div>
+					<Image src="/images/handcrafted_haven_small.webp" width={50} height={50} alt="Handcrafted Haven Logo"/>
+				</div>
+			</Link>
+		);
+}
