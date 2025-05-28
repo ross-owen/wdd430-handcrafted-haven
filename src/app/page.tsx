7724f1384a4d7642c084fa0f3f47c4c26ff5641a
CenterValentine <david@doxy.me>
2025-05-28 14:41:11 -0600

    W03-04 Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/app/ui/hero";
+import FeaturedItems from "@/app/ui/featured-items";
 
 export default function Home() {
   return (
 		<div>
 			<main>
 				<Hero />
+				<FeaturedItems />
 			</main>
+
+				
+
 		</div>
 	);
 }
