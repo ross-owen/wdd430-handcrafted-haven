af526bd455712ee9a1eda00e860e91005e663fe5
jorted-runner <jorted.runner1996@gmail.com>
2025-05-29 21:02:38 -0500

    fixing query route

--- src/app/query/route.ts
+++ src/app/query/route.ts
@@ -3,11 +3,12 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listItems() {
+	const categoryId = 'fa8e1a4d-0e8d-4b20-935d-92cb768e71fa';
 	const data = await sql`
-    SELECT items.name, sellers.first_name
+    SELECT items.title, sellers.first_name
     FROM items
     JOIN sellers ON items.seller_id = sellers.id
-    WHERE items.category_id = fa8e1a4d-0e8d-4b20-935d-92cb768e71fa;
+    WHERE items.category_id = ${categoryId};
   `;
 
 	return data;
