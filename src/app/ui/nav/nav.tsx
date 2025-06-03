4d15e39ac0852e1f18f4bc1d1912fd2d7e1660ad
jorted-runner <jorted.python@gmail.com>
2025-06-03 17:00:08 -0500

    nav bar changes

--- src/app/ui/nav/nav.tsx
+++ src/app/ui/nav/nav.tsx
@@ -1,20 +1,24 @@
+'use client'
+
 import { inter } from '@/app/ui/fonts';
 import Link from 'next/link';
-import Categories from './categories';
 
 export default function Nav() {
     return (
-			<div className="nav-items">
-				<Link href="/" className="nav-links">
-					Home
-				</Link>
-				<Categories />
-				<Link href="/search" className="nav-links">
-					Search
-				</Link>
-				<Link href="/cart" className="nav-links">
-					Cart
-				</Link>
-			</div>
-		);
+		<div className="nav nav-items">
+			<Link href="/" className="nav-links">
+				Home
+			</Link>
+			<Link href="/browse" className="nav-links">
+				Browse
+			</Link>
+			<Link href="/search" className="nav-links">
+				Search
+			</Link>
+			<Link href="/account" className="nav-links">
+				{/* Replace with profile picture */}
+				Account
+			</Link>
+		</div>
+	);
 }
