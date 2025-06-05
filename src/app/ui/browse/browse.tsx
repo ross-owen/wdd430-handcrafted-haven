0fb7e07c803efeef10228bfc0f6453edeca7a6dc
CenterValentine <david@doxy.me>
2025-06-04 20:02:59 -0600

    w04-05 quick update

--- src/app/ui/browse/browse.tsx
+++ src/app/ui/browse/browse.tsx
@@ -1,13 +1,16 @@
-import SearchBar from "./search-bar"
+import SearchBar from "./search-bar";
+import styles from "@/app/ui/browse/browse.module.css";
 
 export default function Catalog() {
-    return (
+  return (
     <>
+      <div className={styles.catalogPage}>
         <h1>Catalog Search</h1>
         <SearchBar />
         <div>
-            <p>Search results will be displayed here.</p>
+          <p>Search results will be displayed here.</p>
         </div>
+      </div>
     </>
-    )
+  );
 }
--- src/app/ui/browse/search-bar.tsx
+++ src/app/ui/browse/search-bar.tsx
@@ -1,9 +1,19 @@
+import SearchResults from "@/app/ui/browse/search-results";
+import styles from "@/app/ui/browse/browse.module.css";
+
 export default function SearchBar() {
-    return (
+  return (
     <>
-    <div>
-        Search Bar
-    </div>
+      <div>
+        <form className={`${styles["search-bar"]}`}>
+          <label htmlFor="search-bar">
+            <input type="text" placeholder="Search for items" />
+          </label>
+        </form>
+      </div>
+      <div className="search-results">
+        <SearchResults />
+      </div>
     </>
-    )
+  );
 }
