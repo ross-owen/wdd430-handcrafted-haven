1aaf2f5f6a01c125c187625982c7dfdc659361ca
jorted-runner <jorted.python@gmail.com>
2025-06-05 13:22:08 -0500

    mobile view styling

--- src/app/ui/browse/results.tsx
+++ src/app/ui/browse/results.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import { fetchFilteredItems } from '@/app/lib/data';
+import styles from '@/app/ui/browse.module.css';
+import StarRating from './star';
 
 export default async function ResultsTable({
 	query,
@@ -11,30 +13,32 @@ export default async function ResultsTable({
 	const items = await fetchFilteredItems(query, currentPage);
 
 	return (
-		<div className="results-container">
+		<div className={`${styles['results-container']}`}>
 			{items?.map((item) => (
-				<div key={item.id} className="results-item">
+				<div key={item.id} className={`${styles['results-item']}`}>
 					<Image
 						src={`/images/${item.image_name}`}
-						width={100}
-						height={100}
+						width={600}
+						height={600}
 						alt={`${item.title} product image`}
-						className="results-image"
+						className={`${styles['results-image']}`}
 					/>
-					<div className="results-details">
+					<div className={`${styles['results-details']}`}>
 						<h2>{item.title}</h2>
-						<p>${item.price}</p>
 						<p>{item.description}</p>
-						<p>
-							<span>Seller:</span> {item.first_name} {item.last_name}
-						</p>
-						<p>
-							<span>Category:</span> {item.category_name}
-						</p>
-						<p>
-							<span>Average Rating:</span>{' '}
-							{Number(item.average_rating).toFixed(1)}
-						</p>
+						<div className={`${styles['results-price-rating']}`}>
+							<p>${item.price}</p>
+							<p>
+								{item.average_rating !== 0 && (
+									<>
+										<StarRating
+											rating={Number(item.average_rating)}
+											maxStars={5}
+										/>
+									</>
+								)}
+							</p>
+						</div>
 					</div>
 				</div>
 			))}
