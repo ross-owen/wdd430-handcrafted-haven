0e47ebe35f16d3c0dc83ccb505086aee3781c8ff
Ross L. Owen <rowen@byupathway.edu>
2025-06-12 20:59:48 -0600

    add seller by id and nav to seller from home page

---
+++ src/app/seller-profile/[sellerId]/page.tsx
@@ -0,0 +1,46 @@
+﻿import {Metadata} from 'next';
+import {Suspense} from "react";
+import SellerProfileDetail from "@/app/ui/seller-profile-detail";
+import ResultsTable from "@/app/ui/browse/results";
+import Link from "next/link";
+import {fetchSellerById} from "@/app/lib/data";
+import styles from "@/app/ui/seller.module.css"
+
+export const metadata: Metadata = {
+  title: 'Seller Profile',
+};
+
+interface SellerPageProps {
+  params: Promise<{
+    sellerId: string;
+  }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function SellerProfilePage({params} : SellerPageProps  ) {
+  const resolvedParams = await params;
+  let seller;
+  try {
+    seller = await fetchSellerById(resolvedParams.sellerId);
+  } catch (error) {
+    console.error("Error retrieving seller details", error);
+    throw error;
+  }
+
+  return (
+      <main>
+        <Suspense>
+          <SellerProfileDetail seller={seller}/>
+        </Suspense>
+        <section>
+          <div className={styles.items}>
+            <h2>{seller.first_name}'s Items</h2>
+          </div>
+          <Suspense>
+            <ResultsTable query={''} seller_id={seller.id} category_id={''} rating={''} currentPage={1}/>
+          </Suspense>
+        </section>
+      </main>
+  );
+
+}
