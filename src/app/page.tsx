326c9419996a380ce46a2f8ad0e7156e409102d2
jorted-runner <jorted.python@gmail.com>
2025-06-13 12:09:07 -0500

    Featured Items update to use results table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/app/ui/hero';
 import SellerSpotlight from '@/app/ui/seller-spotlight';
 import FeaturedItems from '@/app/ui/featured-items';
+import ResultsTable from './ui/browse/results';
 
 export default function Home() {
 	return (
@@ -8,7 +9,7 @@ export default function Home() {
 			<main>
 				<Hero />
 				<SellerSpotlight />
-				<FeaturedItems />
+				<ResultsTable query={''} seller_id={''} category_id={''} random={true} currentPage={1} rating={''} />
 			</main>
 		</div>
 	);
