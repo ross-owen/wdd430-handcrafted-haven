4df684baa883bf546c0ff0581d5841204a16a5d4
jorted-runner <jorted.python@gmail.com>
2025-06-18 11:19:13 -0500

    migration of images to DB as binary.

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,10 +1,22 @@
-﻿export function starRating (rating: number) {
-  const stars = [];
-  for (let i = 0; i < rating; i++) {
-    stars.push("<span>★</span>");
-  }
-  for (let i = rating; i < 5; i++) {
-    stars.push("<span>☆</span>");
-  }
-  return stars.join("");
+﻿import sharp from 'sharp';
+
+export function starRating(rating: number) {
+	const stars = [];
+	for (let i = 0; i < rating; i++) {
+		stars.push('<span>★</span>');
+	}
+	for (let i = rating; i < 5; i++) {
+		stars.push('<span>☆</span>');
+	}
+	return stars.join('');
+}
+
+export async function convertImageToRawBinary(image: File) {
+	const fileBuffer = Buffer.from(await image.arrayBuffer());
+	const optimizedBuffer = await sharp(fileBuffer)
+		.resize({ width: 600, height: 600, fit: 'inside' })
+		.webp({ quality: 75 })
+		.toBuffer();
+
+	return optimizedBuffer;
 }
